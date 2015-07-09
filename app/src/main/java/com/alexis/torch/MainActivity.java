e56147abe13eb28ff11e326e7cf0c3424b640617
Alexis Jacob <alexis.jacob@epitech.eu>
2015-07-09 16:31:44 +0200

    First commit using android studio

---
+++ app/src/main/java/com/alexis/torch/MainActivity.java
@@ -0,0 +1,73 @@
+package com.alexis.torch;
+
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.logging.Logger;
+
+import static android.app.PendingIntent.getActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+    boolean lightOn = false;
+    Camera cam = null;
+    int mCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.light);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               if (!lightOn) {
+
+                    cam = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+                    Camera.Parameters p = cam.getParameters();
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    cam.setParameters(p);
+                    cam.startPreview();
+                    lightOn = true;
+                }
+                else {
+                    cam.stopPreview();
+                    cam.release();
+                    lightOn = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
